917106c73c3b901387b2af891b11b1282c06996d
phiha428 <phiha428@student.otago.ac.nz>
2024-12-22 20:58:21 +1300

    testing new home page

---
+++ src/home.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Home() {
+
+    //Create a homepage for Phillips Music & Tech
+
+    return (
+        <>
+            <h1>Phillips Music & Tech</h1>
+            <p>Welcome to Phillips Music & Tech! We are a small business that specializes in music and technology. We offer a variety of services including music lessons, instrument repair, and computer repair. Our goal is to provide high-quality services at affordable prices. If you have any questions or would like to schedule an appointment, please contact us.</p>
+        </>
+    )
+
+
+}
+
+export default Home
