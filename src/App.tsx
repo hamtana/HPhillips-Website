c02e554bc9db0f06b03a7a15bae2f10da94f97ee
phiha428 <phiha428@student.otago.ac.nz>
2024-12-22 20:49:08 +1300

    testing homepage connection

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
+      // write me some code for a home page
+        <div className="App">
+            <header className="App-header">
+            <img src={reactLogo} className="App-logo" alt="logo" />
+            <img src={viteLogo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            </header>
   )
 }
 
