19f359b455b2d259e05a6499b35602acf1f25ba3
phiha428 <phiha428@student.otago.ac.nz>
2025-06-12 12:42:40 +1200

    made Key Attributes bigger

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -23,7 +23,7 @@ export const AboutSection = () => {
             </p>
 
             <p className="text-muted-foreground">
-              <h3 className="text-1xl font-semibold">Key Attributes</h3>
+              <h3 className="text-2xl font-semibold">Key Attributes</h3>
             </p>
 
               <ul className="list-disc pl-6 text-muted-foreground">
