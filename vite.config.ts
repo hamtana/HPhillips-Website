e91829435e15c2348c5cca18e285340ab6867214
phiha428 <phiha428@student.otago.ac.nz>
2025-06-12 13:34:59 +1200

    fixedup 404 page not showing

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // Ensures Vite dev server also handles client-side routing properly
+    fs: {
+      allow: ['.'],
+    },
+  }
 });
