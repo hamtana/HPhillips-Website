eda851a3a59faa64615f87e93e6744fb24dce545
phiha428 <phiha428@student.otago.ac.nz>
2025-06-12 21:54:31 +1200

    changed the file type to cjs

--- api/send-email.cjs
+++ api/send-email.cjs
@@ -1,6 +1,6 @@
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
-export default async function handler(req: any, res: any) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Only POST requests allowed' });
   }
@@ -32,4 +32,4 @@ export default async function handler(req: any, res: any) {
     console.error('Error sending email:', err);
     res.status(500).json({ message: 'Failed to send email' });
   }
-}
+};
