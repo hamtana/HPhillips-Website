2d8cd8002df2cdd07a9254a55a7919840abb3619
phiha428 <phiha428@student.otago.ac.nz>
2025-06-12 12:35:31 +1200

    added music projects, fixed About Section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User } from "lucide-react";
+import { Briefcase, Code, Contact, Music, User } from "lucide-react";
 
 export const AboutSection = () => {
   return (
@@ -12,21 +12,24 @@ export const AboutSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <h3 className="text-2xl font-semibold">
-              Passionate Web Developer & Tech Creator
+              Passionate Software Engineer and Musician 
             </h3>
 
             <p className="text-muted-foreground">
-              With over 5 years of experience in web development, I specialize
-              in creating responsive, accessible, and performant web
-              applications using modern technologies.
+              I’m a recent graduate from the University of Otago with a passion for both software engineering and music production. 
+              I take pride in writing clean, efficient code and producing high-quality audio.<br></br><br></br> Whether I’m developing an app or composing a track,
+              I approach every project with creativity, precision, and a strong work ethic. I enjoy solving problems, learning new tools and technologies, 
+              and collaborating with others to bring ideas to life. ,<br></br> I'm excited to contribute to projects that blend tech and creativity—and to grow as both an engineer and a musician.
             </p>
 
             <p className="text-muted-foreground">
-              I'm passionate about creating elegant solutions to complex
-              problems, and I'm constantly learning new technologies and
-              techniques to stay at the forefront of the ever-evolving web
-              landscape.
+              <h3 className="text-1xl font-semibold">Key Attributes</h3>
             </p>
+
+              <ul className="list-disc pl-6 text-muted-foreground">
+                <li><b>Time Management</b> -  I thrive in a busy environment and have become highly skilled at managing my time, balancing university studies, three job, and my music commitments and interests.</li>
+                <li><b>Problem Solver </b> - My substantial capacity for creative critical, and computational thinking makes me a highly effective problem solver in the development of packages and maintenance of existing systems.</li>
+              </ul>
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">
@@ -35,7 +38,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="/cv/HamishPhillips_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
@@ -51,10 +54,10 @@ export const AboutSection = () => {
                   <Code className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg"> Web Development</h4>
+                  <h4 className="font-semibold text-lg"> Software Development</h4>
                   <p className="text-muted-foreground">
-                    Creating responsive websites and web applications with
-                    modern frameworks.
+                    Proficient in a variety of coding Languages for building
+                    responsive and interactive applications, as well as maintenance of existing systems.
                   </p>
                 </div>
               </div>
@@ -67,10 +70,10 @@ export const AboutSection = () => {
                   <User className="h-6 w-6 text-primary" />
                 </div>
                 <div className="text-left">
-                  <h4 className="font-semibold text-lg">UI/UX Design</h4>
+                  <h4 className="font-semibold text-lg">People Focussed</h4>
                   <p className="text-muted-foreground">
-                    Designing intuitive user interfaces and seamless user
-                    experiences.
+                    Always strive to understand user needs and deliver
+                    solutions that enhance user experience and satisfaction.
                   </p>
                 </div>
               </div>
@@ -92,6 +95,40 @@ export const AboutSection = () => {
                 </div>
               </div>
             </div>
+
+            {/* Music Production */}
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Music className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Music Production</h4>
+                  <p className="text-muted-foreground">
+                    High Attention to detail in all aspects of music production,
+                    from composition to mixing and mastering.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Music Production */}
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Contact className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Effective Communicator & Collaborator</h4>
+                  <p className="text-muted-foreground">
+                    Able to convey complex ideas clearly and work effectively with
+                    diverse teams to achieve common goals.
+                  </p>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -3,12 +3,13 @@
 
 import { ArrowRight, ExternalLink, Github } from "lucide-react"
 
+// List of software projects
 const projects = [
     {
         id: 1,
         title: "PantryPal",
-        description: "A website developed with a team from University which uses AI to produce recipes, based off items that you have added to your Pantry",
-        image: "tbc",
+        description: "A website developed with a team from University which uses AI to produce recipes, based off items that you have added to your Pantry.",
+        image: "/projects/project1.png",
         tags: ["React", "Django Python"],
         demoUrl: "#",
         githubUrl: "https://github.com/hamtana/PantryPal/tree/master"
@@ -17,8 +18,8 @@ const projects = [
     {
         id: 2,
         title: "Phillips Music & Tech Website",
-        description: "A website developed to show my portfolio of work, and let people get in contact with me regarding self-employed work",
-        image: "../",
+        description: "A website developed to show my portfolio of work, and let people get in contact with me regarding self-employed work.",
+        image: "/projects/project2.png",
         tags: ["Vite+React", "Vercel", "TailwindCSS"],
         demoUrl: "https://phillipsmusictech.co.nz",
         githubUrl: "https://github.com/hamtana/HPhillips-Website"
@@ -27,7 +28,7 @@ const projects = [
     {
         id: 3,
         title: "CCDHB Uniform Tracker",
-        description: "In development, building an Inventory and Uniform issuing and management system for Wellington Regional Hospital",
+        description: "In development, building an inventory, uniform issuing and management system for Wellington Regional Hospital.",
         image: "tbc",
         tags: ["React", "Java Springboot", "PostgreSQL"],
         demoUrl: "#",
@@ -37,12 +38,44 @@ const projects = [
 
 ]
 
+// List of music projects
+const musicProjects = [
+    {
+        id: 1,
+        title: "Floating",
+        description: "Song for my band, DownsideUp to be released in 2025. Progress mix available here.",
+        tags: ["Logic Pro", "Pro Tools"],
+        audioSrc: "/audio/Floating.mp3",
+       
+    },
+
+    {
+        id: 2,
+        title: "Undertow",
+        description: "Song recorded in 2024 as part of a university project.",
+        tags: ["Logic Pro", "Pro Tools"],
+        audioSrc: "/audio/Undertow.mp3",
+        
+    },
+
+    {
+        id: 3,
+        title: "Runaway",
+        description: "Song produced for my band DownsideUp, to be released in 2025.",
+        tags: ["Logic Pro", "Pro Tools"],
+        audioSrc: "/audio/Runaway.mp3",
+        
+    }
+
+
+]
+
 
 export const ProjectsSection = () => {
     return <section id="projects" className="py-24 px-4 relative">
         <div className="container mx-auto max-w-5xl">
-            <h2 className="text-3xl md:text-4xl font-bold"> Featured <span className="text-primary"> Projects </span> </h2>
-            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto"> Here are some of my recent and current projects</p>
+            <h2 className="text-3xl md:text-4xl font-bold"> Featured <span className="text-primary"> Software Projects </span> </h2>
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto"> Here are some of my recent and current software projects</p>
             {/* Projects to be displayed below */}
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -85,6 +118,7 @@ export const ProjectsSection = () => {
                     </div>
                 ))}
             </div>
+
             
             {/* Adding GitHub Link */}
             <div className="text-center mt-12">
@@ -92,5 +126,51 @@ export const ProjectsSection = () => {
             </div>
 
         </div>
+        
+        {/* Music Production Project Section */}
+        <div className="container py-24 px-4 mx-auto max-w-5xl">
+            <h2 className="text-3xl md:text-4xl font-bold"> Featured <span className="text-primary"> Music Projects </span> </h2>
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto"> Here are some of my recent and current music projects</p>
+            {/* Projects to be displayed below */}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {musicProjects.map((project, key) => (
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+
+
+                        {/* Project Title */}
+                        <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+
+                        {/* Project Audio Player */}
+                        <div className="mt-4">
+                        <audio controls className="w-full rounded">
+                            <source src={project.audioSrc} type="audio/mpeg" />
+                            Your browser does not support the audio element.
+                        </audio>
+                        </div>
+
+                        {/* Adding Project Tags */}
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag)=> (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-secondary text-secondary-foreground">{tag}</span>
+                                ))}
+                            </div>
+                        </div>
+                        
+
+                        {/* Project Description */}
+                        <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+
+
+                    
+                    </div>
+                ))}
+            </div>
+        </div>
+
+
+         
+
     </section>
 }
